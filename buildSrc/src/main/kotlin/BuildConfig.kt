52b5495e679ed269a4f34c5a0c616d208bfa620e
uku <hi@uku.moe>
2026-06-16 19:12:11 +0200

    chore: bump version to 1.8.0

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val NEOFORGE_VERSION: String = "26.2.0.0-beta"
     const val UKULIB_VERSION: String = "2.1.0+26.2"
 
-    const val MOD_VERSION: String = "1.7.0"
+    const val MOD_VERSION: String = "1.8.0"
 
     const val MODRINTH_PROJECT_ID: String = "b9olMQ2D"
 
